823624940662511403e6f5f194d80e7e104dcbba
veasnapovsr <veasnavarapov@gmail.com>
2024-10-27 23:06:37 +0700

    Update w3_s2_Ex2.dart

--- W3_S2/w3_s2_Ex2.dart
+++ W3_S2/w3_s2_Ex2.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  
   runApp(
+    
     MaterialApp(
       home: Container(
         color: Colors.blue[300],
@@ -11,19 +13,28 @@ void main() {
           decoration: BoxDecoration(
             color: Colors.blue[800],
             borderRadius: BorderRadius.circular(20.0),
+            
           ),
+          
           child: Center(
             child: Text(
+              
               "CADT STUDENTS",
+              
               style: TextStyle(
                 fontSize: 30,
                 color: Colors.white,
-                decoration: TextDecoration.none, 
+                decoration: TextDecoration.none, //To set no Underline 
+                
               ),
             ),
           ),
         ),
+        
       ),
+      
     ),
+    
   );
+  
 }
