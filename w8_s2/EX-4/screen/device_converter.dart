df47279a971b9947b1893d1ae8d8dda27d2dc451
veasnapovsr <veasnavarapov@gmail.com>
2024-12-05 00:15:11 +0700

    Add files via upload

---
+++ w8_s2/EX-4/screen/device_converter.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+ 
+class DeviceConverter extends StatefulWidget {
+  const DeviceConverter({super.key});
+
+  @override
+  State<DeviceConverter> createState() => _DeviceConverterState();
+}
+
+class _DeviceConverterState extends State<DeviceConverter> {
+ 
+  final BoxDecoration textDecoration = BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(12),
+  );
+ 
+
+  @override
+  void initState() {
+    super.initState();
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Icon(
+            Icons.money,
+            size: 60,
+            color: Colors.white,
+          ),
+          const Center(
+            child: Text(
+              "Converter",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+          const SizedBox(height: 50),
+          const Text("Amount in dollars:"),
+          const SizedBox(height: 10),
+
+          TextField(
+              decoration: InputDecoration(
+                  prefix: const Text('\$ '),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.white, width: 1.0),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Enter an amount in dollar',
+                  hintStyle: const TextStyle(color: Colors.white)),
+              style: const TextStyle(color: Colors.white)),
+
+          const SizedBox(height: 30),
+          const Text("Device: (TODO !)"),
+       
+
+          const SizedBox(height: 30),
+          const Text("Amount in selected device:"),
+          const SizedBox(height: 10),
+          Container(
+              padding: const EdgeInsets.all(10),
+              decoration: textDecoration,
+              child: const Text('TODO !'))
+        ],
+      )),
+    );
+  }
+}
